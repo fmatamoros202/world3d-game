5940f08efdc7c89438c483b39c36304f56f3f5a9
Fernando Matamoros <fmatamoros202@gmail.com>
2022-01-11 17:22:34 -0500

    created a plane and box geometry and added physics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+import { Physics, useBox, usePlane } from '@react-three/cannon';
+
+function Box(props){
+
+  const [ref, api] = useBox(()=>({mass:1, position:[0,5,0]}));
+
+  return(
+    <mesh ref={ref}  onClick={()=>{api.velocity.set(0,10,0)}}>
+      <boxBufferGeometry attach='geometry' args={props.size}/>
+      <meshLambertMaterial attach='material' color='blue' wireframe={props.invisible}/>
+    </mesh>
+  )
+}
+
+function Plane(props){
+
+  const [ref]= usePlane(()=>({rotation: [-Math.PI/2,0,0]}))
+
+  return(
+    <mesh position={[0,0,0]} ref={ref} rotation={[-Math.PI/2,0,0]}>
+      <planeBufferGeometry attach='geometry' args={[100,100]} />
+      <meshLambertMaterial attach='material' color='lightgreen'/>
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <OrbitControls/>
+      <Stars/>
+      <ambientLight intensity={0.5}/>
+      <spotLight position={[10,15,10]} angle={0.3}/>
+      <Physics>
+        <Box size={[1,1,1]} invisible={true}></Box>
+        {/* <Box size={[1,1,1]} invisible={false}></Box> */}
+        <Plane/>
+      </Physics>
+    </Canvas>
   );
 }
 

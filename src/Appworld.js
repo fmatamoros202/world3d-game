c7ea6f1fa33a5c3cfbb025f1660208bbca8f4fcf
Fernando Matamoros <fmatamoros202@gmail.com>
2022-01-12 15:18:16 -0500

    Added folders and files for components

--- src/Appworld.js
+++ src/Appworld.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import './App.css';
+import './Appworld.scss';
 import { Canvas } from '@react-three/fiber';
 import {  Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 
+import Plane from './Components/Plane/Plane';
+import Player from './Components/Player/Player';
 
 
-function App() {
+
+function Appworld() {
   return (
     <Canvas shadows sRGB>
       <Sky sunPosition={[100,20,100]}/>
       <ambientLight intensity={0.25}/>
       <pointLight castShadow intensity={0.7} position={[100,100,100]} />
       <Physics>
-        <Box size={[10,10,10]} invisible={true}></Box>
-        {/* <Box size={[1,1,1]} invisible={false}></Box> */}
+        <Player/>
         <Plane/>
       </Physics>
     </Canvas>
   );
 }
 
-export default App;
+export default Appworld;

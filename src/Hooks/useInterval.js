b918ebfaa8c149db21f27d594d4ea49517aa5e2c
Fernando Matamoros <fmatamoros202@gmail.com>
2022-01-12 17:21:14 -0500

    Added special hooks to the functionality

---
+++ src/Hooks/useInterval.js
@@ -0,0 +1,24 @@
+import { useRef, useEffect } from "react";
+
+export const useInterval = (callback, delay)=>{
+    const savedCallback = useRef();
+    //remember the latest callback.
+
+    useEffect(()=>{
+        savedCallback.current = callback;
+    });
+
+    useEffect(()=>{
+        function tick() {
+            if (typeof savedCallback?.current !== 'undefined'){
+                savedCallback.current();
+            }
+        }
+        if (delay !== null){
+            const id = setInterval(tick, delay);
+            return () => {
+                clearInterval(id)
+            }
+        }
+    },[delay]);
+}

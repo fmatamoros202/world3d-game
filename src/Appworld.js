9e4ef458d850ef4ab0712d464c67538a7333c57c
Fernando Matamoros <fmatamoros202@gmail.com>
2022-01-12 15:10:24 -0500

    created a remote repo

--- src/Appworld.js
+++ src/Appworld.js
@@ -8,11 +8,10 @@ import { Physics } from '@react-three/cannon';
 
 function App() {
   return (
-    <Canvas>
-      <OrbitControls/>
-      <Stars/>
-      <ambientLight intensity={0.5}/>
-      <spotLight position={[10,15,10]} angle={0.3}/>
+    <Canvas shadows sRGB>
+      <Sky sunPosition={[100,20,100]}/>
+      <ambientLight intensity={0.25}/>
+      <pointLight castShadow intensity={0.7} position={[100,100,100]} />
       <Physics>
         <Box size={[10,10,10]} invisible={true}></Box>
         {/* <Box size={[1,1,1]} invisible={false}></Box> */}

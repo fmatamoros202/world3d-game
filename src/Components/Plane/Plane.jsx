0baa68012fc9cede7769e667db69cdb78118b209
Fernando Matamoros <fmatamoros202@gmail.com>
2022-01-21 10:58:48 -0500

    added the functionality to add a cube anywhere

--- src/Components/Plane/Plane.jsx
+++ src/Components/Plane/Plane.jsx
@@ -3,6 +3,7 @@ import { usePlane } from '@react-three/cannon';
 import { TextureLoader, RepeatWrapping} from 'three';
 import grass from '../../images/grass.jpeg';
 import { useLoader } from '@react-three/fiber';
+import { useStore } from '../../Hooks/useStore';
 
 function Plane(props) {
 
@@ -12,8 +13,21 @@ function Plane(props) {
     planeTexture.wrapT = RepeatWrapping;
     planeTexture.repeat.set(125,125);
 
+    const [addCube,activeTexture] = useStore((state)=>[
+        state.addCube,
+        state.texture,
+    ]);
+
     return (
-        <mesh ref={ref} receiveShadow >
+        <mesh ref={ref} receiveShadow 
+            onClick={(e)=>{
+                e.stopPropagation();
+                const [x,y,z] = Object.values(e.point).map((coord)=>
+                    Math.ceil(coord),
+                );
+                addCube(x,y,z, activeTexture);
+            }}
+        >
             <planeBufferGeometry attach='geometry' args={[100,100]} />
             <meshStandardMaterial map={planeTexture} attach='material' />
         </mesh>

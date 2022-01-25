63495269cd90d9dd7f162d5b087e9d1297eadd62
Fernando Matamoros <fmatamoros202@gmail.com>
2022-01-25 10:11:04 -0500

    Added a way to import blender files

--- src/Appworld.js
+++ src/Appworld.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { Suspense, useRef } from 'react';
 import './Appworld.css';
 import { Canvas } from '@react-three/fiber';
-import {  Sky, OrbitControls } from '@react-three/drei';
+import {  Sky, OrbitControls, useGLTF } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
+import iphone from './objects/iphone2.gltf';
+import Iphone2 from './objects/Iphone2.jsx';
+
+
 import Reset from './Components/Reset/Reset';
 import Instructions from './Components/Instructions/Instructions';
 
@@ -12,6 +16,20 @@ import Cube from './Components/Cube/Cube';
 
 import { useStore } from './Hooks/useStore';
 import { useInterval } from './Hooks/useInterval';
+
+
+function Iphone({ ...props }) {
+    const group = useRef()
+    const { nodes, materials } = useGLTF(iphone)
+    return (
+      <group ref={group} {...props} dispose={null}>
+        <group rotation={[0, -Math.PI / 2, -Math.PI / 2]} scale={[2, 1, 1]}>
+          <mesh geometry={nodes.Plane_1.geometry} material={materials['Material.005']} />
+          <mesh geometry={nodes.Plane_2.geometry} material={materials['Material.004']} />
+        </group>
+      </group>
+    )
+  }
 
 
 
@@ -33,17 +51,21 @@ function Appworld() {
     <>
         <Reset />
         <Canvas shadows sRGB>
-        {/* <OrbitControls/> */}
-        <Sky sunPosition={[100,20,100]}/>
-        <ambientLight intensity={0.25}/>
-        <pointLight castShadow intensity={0.7} position={[100,100,100]} />
-        <Physics gravity={[0,-30,0]}>
-            <Plane position={[0,0.5,0]}/>
-            <Player position={[0,3,10]}/>
-            {cubes.map((cube)=>( 
-                <Cube receiveShadow position={cube.pos} texture={cube.texture} args={[2,2,2]}/>
-            ))}
-        </Physics>
+            {/* <OrbitControls/> */}
+            <Sky sunPosition={[100,20,100]}/>
+            <ambientLight intensity={0.25}/>
+            <pointLight castShadow intensity={0.7} position={[100,100,100]} />
+            <Physics gravity={[0,-30,0]}>
+                <Plane position={[0,0,0]}/>
+                <Player position={[0,3,10]}/>
+                {cubes.map((cube)=>( 
+                    <Cube receiveShadow position={cube.pos} texture={cube.texture} args={[2,2,2]}/>
+                ))}
+            {/* <Suspense fallback={null}> */}
+                <Iphone position={[0,7,-1]}/>
+            {/* </Suspense> */}
+            <Iphone2 position={[3,7,-1]}/>
+            </Physics>
         </Canvas>
         <Instructions/>
     </>
